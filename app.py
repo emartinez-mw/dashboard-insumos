68a1af0fc150e0d8a0dce03680d1f7e37510d3f5
Ezequiel Martinez <emartinez@admin.com.ar>
2026-07-03 14:09:42 -0300

    revert: sacar debug temporal #2, dejar require_login limpio en app.py

--- app.py
+++ app.py
@@ -2,16 +2,6 @@ import streamlit as st
 
 from access_control import require_login
 
-st.write("DEBUG top-level secrets keys:", list(st.secrets.keys()))
-if "auth" in st.secrets:
-    auth_val = st.secrets["auth"]
-    st.write("DEBUG type of auth value:", type(auth_val).__name__)
-    try:
-        st.write("DEBUG auth section keys:", list(auth_val.keys()))
-    except Exception as e:
-        st.write("DEBUG auth is not dict-like:", repr(e))
-st.stop()
-
 require_login()
 
 pg = st.navigation(
